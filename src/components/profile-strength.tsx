ce60388d40890693768643ff0f002b837ebea8e5
Rahul Naik <dyrahulnaik22@gmail.com>
2025-12-08 05:37:10 +0000

    in profile strength the up side growing icon i want it in #f72585 color

--- src/components/profile-strength.tsx
+++ src/components/profile-strength.tsx
@@ -97,7 +97,7 @@ export function ProfileStrength({ user }: ProfileStrengthProps) {
         <div className="p-4 border rounded-lg bg-card space-y-4">
             <div className="flex items-center justify-between">
                 <p className="font-semibold text-lg">Profile Strength</p>
-                <TrendingUp className="h-5 w-5 text-primary" />
+                <TrendingUp className="h-5 w-5 text-[#f72585]" />
             </div>
             <Separator />
             <div>
