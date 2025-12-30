e4c46ae9a32a5172d5579cb2f80207b8f3a3aa53
Rahul Naik <dyrahulnaik22@gmail.com>
2025-12-30 15:09:31 +0000

    Tracking Prevention blocked access to storage for https://cdnjs.cloudfla

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={cn('relative h-full font-sans antialiased', inter.variable)}>
         <UserProvider>
