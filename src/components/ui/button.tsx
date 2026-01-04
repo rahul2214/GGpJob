5a2015a2069dcba18f0ba51e0b782bbddd6369e9
Rahul Naik <dyrahulnaik22@gmail.com>
2026-01-04 15:23:31 +0000

    Remove the background color for google button and make it as white

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        google: "bg-background text-foreground border hover:bg-accent/50",
+        google: "bg-white text-foreground border hover:bg-accent/50",
       },
       size: {
         default: "h-10 px-4 py-2",
